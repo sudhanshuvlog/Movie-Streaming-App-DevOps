8cbb39f7886309a288a9ef6419667d67f33e36de
Trainersudhanshu <sudhanshutest123@gmail.com>
2025-09-21 13:48:41 +0530

    Updated the variables

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const upload = multer({
 // Helper function to upload files to S3
 const uploadToS3 = async (fileBuffer, fileName, contentType) => {
   const uploadParams = {
-    Bucket: 'moviebucketsudhanshuvlog',
+    Bucket: 'gfg37moviebucket',
     Key: fileName,
     Body: fileBuffer,
     ContentType: contentType,
